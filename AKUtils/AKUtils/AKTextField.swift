64fd009992335bad2d6f77fa1c657babeacdf7a1
Artem Kalmykov <ar.kalmykov@yahoo.com>
2018-09-26 15:59:29 +0300

    Add insets for AKTextField.

--- AKUtils/AKUtils/AKTextField.swift
+++ AKUtils/AKUtils/AKTextField.swift
@@ -11,9 +11,15 @@ import UIKit
 open class AKTextField: UITextField {
     @IBOutlet open weak var nextField: UIResponder?
     
-    @IBInspectable var handleReturn: Bool = false
-    
-    public var rightViewInset: UIEdgeInsets = .zero
+    @IBInspectable public var handleReturn: Bool = false
+    @IBInspectable public var isPasteEnabled: Bool = true
+    @IBInspectable public var isSelectEnabled: Bool = true
+    @IBInspectable public var isSelectAllEnabled: Bool = true
+    @IBInspectable public var isCopyEnabled: Bool = true
+    @IBInspectable public var isCutEnabled: Bool = true
+    @IBInspectable public var isDeleteEnabled: Bool = true
+    @IBInspectable public var rightViewInset: UIEdgeInsets = .zero
+    @IBInspectable public var textInset: UIEdgeInsets = .zero
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -39,5 +45,27 @@ open class AKTextField: UITextField {
     
     override open func rightViewRect(forBounds bounds: CGRect) -> CGRect {
         return super.rightViewRect(forBounds: bounds).inset(by: rightViewInset)
+    }
+    
+    override open func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        switch action {
+        case #selector(UIResponderStandardEditActions.paste(_:)) where !isPasteEnabled,
+             #selector(UIResponderStandardEditActions.select(_:)) where !isSelectEnabled,
+             #selector(UIResponderStandardEditActions.selectAll(_:)) where !isSelectAllEnabled,
+             #selector(UIResponderStandardEditActions.copy(_:)) where !isCopyEnabled,
+             #selector(UIResponderStandardEditActions.cut(_:)) where !isCutEnabled,
+             #selector(UIResponderStandardEditActions.delete(_:)) where !isDeleteEnabled:
+            return false
+        default:
+            return super.canPerformAction(action, withSender: sender)
+        }
+    }
+    
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textInset)
+    }
+    
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return self.textRect(forBounds: bounds)
     }
 }
