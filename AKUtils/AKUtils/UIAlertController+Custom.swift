d22077962704282b8494ea03153e4583cd5eda91
Artem Kalmykov <ar.kalmykov@yahoo.com>
2018-09-26 16:59:10 +0300

    Add UIAlertController extension.

---
+++ AKUtils/AKUtils/UIAlertController+Custom.swift
@@ -0,0 +1,49 @@
+//
+//  UIAlertController+Custom.swift
+//  FFControls
+//
+//  Created by Artem Kalmykov on 10/12/16.
+//  Copyright © 2016 Faifly. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController{
+    public static func showOKAlert(title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        alertController.show()
+    }
+    
+    private struct AssociatedKeys {
+        static var AlertWindow = "alertWindow"
+    }
+    
+    private var alertWindow: UIWindow? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.AlertWindow) as? UIWindow? ?? nil
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.AlertWindow, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    public func show() {
+        self.alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        self.alertWindow?.rootViewController = UIViewController()
+        
+        let topWindow = UIApplication.shared.windows.last
+        self.alertWindow?.windowLevel = topWindow!.windowLevel + 1
+        
+        self.alertWindow?.makeKeyAndVisible()
+        self.alertWindow?.rootViewController?.present(self, animated: true, completion: nil)
+    }
+    
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        self.alertWindow?.isHidden = true
+        self.alertWindow = nil
+    }
+}
