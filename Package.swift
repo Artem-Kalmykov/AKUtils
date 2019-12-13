00ebb9aae0217ccb7674021e355fca789827aac5
Artem Kalmykov <ar.kalmykov@gmail.com>
2019-12-13 19:19:59 +0200

    Transition to SPM.

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "AKUtils",
+    products: [
+        .library(
+            name: "AKUtils",
+            targets: ["AKUtils"]),
+    ],
+    targets: [
+        .target(
+            name: "AKUtils",
+            dependencies: [])
+    ]
+)
